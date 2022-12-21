b2178c0c14ffe31cde518ad69848c2b20e6bbd76
Tobias Toland <github@thinktt.e4ward.com>
2022-12-20 19:58:28 -0600

    basic movement and css for promotion overlay

--- chessboard.js
+++ chessboard.js
@@ -24,11 +24,13 @@ const config = {
     enabled: false,
   },
 }
+
 const cg = Chessground(document.getElementById('chessground'), config);
 window.cg = cg
+window.game = game
 
 
-function onMove(from, to) {
+async function onMove(from, to) {
   console.log(from, to)
   game.move({ from, to })
   cg.set({
@@ -37,6 +39,21 @@ function onMove(from, to) {
       color: getTurn(game),
       dests: getLeglaMoves(game),
     }
+  })
+}
+
+function goBack() {
+  game.undo()
+  const lastMove = getLastMove(game)
+  cg.set({ 
+    fen: game.fen(),
+    check: game.in_check(),
+    movable: {
+      color: getTurn(game),
+      dests: getLeglaMoves(game),
+    },
+    turnColor: getTurn(game),
+    lastMove,
   })
 }
 
@@ -54,4 +71,31 @@ function getTurn(game) {
   return (game.turn() === 'w') ? 'white' : 'black';
 }
 
+function getLastMove(game) {
+  const moves = game.history({ verbose: true })
+  const lastMove = moves.length ? moves.pop() : null
+  if (!lastMove) return null
 
+  const { from, to } = lastMove 
+  return [ from, to ]
+}
+
+const goBackbutton = document.querySelector('#go-back-button')
+goBackbutton.addEventListener('click', goBack)
+
+
+const promotionChoiceEl = `
+ <div id="promotion-choice" class="bottom">
+  <square style="top: 87.5%;left: 62.5%">
+    <piece class="queen white"></piece>
+  </square>
+  <square style="top: 75%;left: 62.5%">
+    <piece class="knight white"></piece>
+  </square>
+  <square style="top: 62.5%;left: 62.5%">
+    <piece class="rook white"></piece>
+  </square>
+  <square style="top: 50%;left: 62.5%">
+    <piece class="bishop white"></piece>
+  </square></div>
+`
