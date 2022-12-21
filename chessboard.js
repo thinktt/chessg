61c5023303bd5f1a04486c2120de63718a8a658a
Tobias Toland <github@thinktt.e4ward.com>
2022-12-20 23:31:50 -0600

    overlay mostly working, needs promote functions

--- chessboard.js
+++ chessboard.js
@@ -1,4 +1,5 @@
 import { Chessground } from './node_modules/chessground/chessground.js';
+import { html } from './pageTools.js'
 
 
 const game = new Chess()
@@ -31,18 +32,34 @@ window.game = game
 
 
 async function onMove(from, to) {
-  console.log(from, to)
-  game.move({ from, to })
+  let promotion = null
+  if (isPromotion(from, to)) {
+    promotion = await getUserPromotion(to)
+    cg.set({ animation: { enabled: false } })
+  }
+
+  console.log(from, to, promotion)
+  game.move({ from, to, promotion})
   cg.set({
+    fen: game.fen(),
     check: game.in_check(),
     movable: {
       color: getTurn(game),
       dests: getLeglaMoves(game),
-    }
+    },
   })
+
+  cg.set({ animation: { enabled: true } })
 }
 
 function goBack() {
+  const lastAlgebraMove = game.history().slice(-1)[0]
+  console.log(lastAlgebraMove)
+  if (lastAlgebraMove && lastAlgebraMove.includes('=')) {
+    console.log('Howdy')
+    cg.set({ animation: { enabled: false } })
+  }
+
   game.undo()
   const lastMove = getLastMove(game)
   cg.set({ 
@@ -55,6 +72,8 @@ function goBack() {
     turnColor: getTurn(game),
     lastMove,
   })
+
+  cg.set({ animation: { enabled: true } })
 }
 
 
@@ -83,19 +102,72 @@ function getLastMove(game) {
 const goBackbutton = document.querySelector('#go-back-button')
 goBackbutton.addEventListener('click', goBack)
 
+function isPromotion(fromSquare, toSquare) {
+  const squareState = game.get(fromSquare)
+  if (squareState.type !== 'p') return false
+  if (toSquare.includes('8') || toSquare.includes('1')) return true
+  return false
+}
 
-const promotionChoiceEl = `
- <div id="promotion-choice" class="bottom">
-  <square style="top: 87.5%;left: 62.5%">
-    <piece class="queen white"></piece>
-  </square>
-  <square style="top: 75%;left: 62.5%">
-    <piece class="knight white"></piece>
-  </square>
-  <square style="top: 62.5%;left: 62.5%">
-    <piece class="rook white"></piece>
-  </square>
-  <square style="top: 50%;left: 62.5%">
-    <piece class="bishop white"></piece>
-  </square></div>
-`
+
+
+
+async function getUserPromotion(toSquare) {
+
+  const column = toSquare[0]
+
+  const offSetMap = {
+    'a' : 0,
+    'b' : 12.5,
+    'c' : 24.5,
+    'd' : 37,
+    'e' : 49.25,
+    'f' : 61.75,
+    'g' : 74.25,
+    'h' : 86.5,
+  }
+  const leftOffset = offSetMap[column]
+
+  let color = 'black'
+  let queenTop = 74
+  let topOffsetIncrement = -12.5
+  
+  if (toSquare.includes('8')) { 
+    color = 'white'
+    queenTop = 0
+    topOffsetIncrement = 12.5
+  }
+
+
+  const knightTop = queenTop + topOffsetIncrement
+  const roookTop = knightTop + topOffsetIncrement
+  const bishopTop = roookTop + topOffsetIncrement
+
+  const promotionChoiceEl = html`
+    <div class="promotion-overlay cg-wrap">
+      <square style="top:${queenTop}%; left: ${leftOffset}%">
+        <piece class="queen ${color}"></piece>
+      </square>
+      <square style="top:${knightTop}%; left: ${leftOffset}%">
+        <piece class="knight ${color}"></piece>
+      </square>
+      <square style="top:${roookTop}%; left: ${leftOffset}%">
+        <piece class="rook ${color}"></piece>
+      </square>
+      <square style="top:${bishopTop}%; left: ${leftOffset}%">
+        <piece class="bishop ${color}"></piece>
+      </square>
+    </div>
+  `
+
+  const boardContainerEl = document.querySelector('.board-container')
+  boardContainerEl.insertAdjacentHTML('beforeend', promotionChoiceEl)
+
+  return 'q'
+}
+
+
+
+function promote(square, piece) {
+
+}
